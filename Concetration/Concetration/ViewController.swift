f1e5107d90591614b2eecf1dffbc287711c9b32c
Yury Sinev <yurysinev@Yurys-MacBook-Pro.local>
2020-01-31 20:12:12 +0400

    Fixed unhandled error (UIButton array missed one of the buttons), added new emojies and cards

--- Concetration/Concetration/ViewController.swift
+++ Concetration/Concetration/ViewController.swift
@@ -8,71 +8,57 @@
 
 import UIKit
 
-enum Emoji: String, CaseIterable {
-    case smile = "😀"
+enum Emoji: String {
     case cry = "😭"
     case scream = "😱"
-    case angel = "😇"
-    case tease = "😛"
-    case love = "😍"
-    case cool = "😎"
-    case botan = "🤓"
-    
-    
+    case raisedEyebrow = "🤨"
+    case halo = "😇"
+    case goofy = "🤪"
+    case skull = "💀"
 }
 
 class ViewController: UIViewController {
     
     private let countLabelText = "Счетчик нажатий:"
-    
-    private var count = 0 {
-        didSet {
-            self.countLabel.text = "\(countLabelText) \(self.count)"
-        }
-    }
-    
-    lazy var game = Concentration(numberOfPairsCard: (self.cardButtons.count + 1) / 2)
+    private var count = 0
     
     @IBOutlet var cardButtons: [UIButton]!
     
-    var emojiArray: [Emoji] = Emoji.allCases
-    var emoji: [Int: Emoji] = [:]
+    var emojiArray: [Emoji] = [.cry, .scream,
+                               .raisedEyebrow, .halo,
+                               .goofy, .skull,
+                                .cry, .scream,
+                                .raisedEyebrow, .halo,
+                                .goofy, .skull]
     
     @IBOutlet weak var countLabel: UILabel!
     
     @IBAction func emojiButtonAction(_ sender: UIButton) {
         
         if let index = self.cardButtons.firstIndex(of: sender) {
-            self.game.chooseCard(at: index)
-            self.updateViewModel()
+            let emoji = self.emojiArray[index]
+            self.flipCard(withEmoji: emoji.rawValue, on: sender)
         } else {
             print("Unhandled Error!!!")
         }
         
+        
     }
     
-    func updateViewModel() {
-        for index in self.cardButtons.indices {
-            let button = self.cardButtons[index]
-            let card = self.game.cards[index]
-            
-            if card.isFaceUp {
-                button.setTitle(self.emoji(for: card), for: .normal)
-                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            } else {
-                button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.6910475492, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.6910475492, blue: 0, alpha: 1)
-            }
-        }
-    }
-    
-    func emoji(for card: Card) -> String {
-        if self.emoji[card.identifier] == nil, self.emojiArray.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(self.emojiArray.count)))
-            self.emoji[card.identifier] = self.emojiArray.remove(at: randomIndex)
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+        self.count += 1
+        
+        self.countLabel.text = "\(countLabelText) \(self.count)"
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.6910475492, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
         
-        return self.emoji[card.identifier]?.rawValue ?? "?"
     }
     
 }
